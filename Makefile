93917c8cd3f6230c0ce65bf942116ed0fd9665d5
k.barylnikov <ipt_git_bot@ipt.ru>
2025-11-17 14:00:03 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,8 @@
+lint: ruff-format ruff
+
+ruff:
+	ruff check --fix
+
+ruff-format:
+	ruff format
+
---
+++ blog/views.py
@@ -0,0 +1,65 @@
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
+
+from mysite import settings
+
+from .forms import EmailPostForm
+from .models import Post
+
+# def post_list(request):
+#     all_posts = Post.published.all()
+#     # Постраничная разбивка с 3 постами на страницу
+#     paginator = Paginator(all_posts, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # Если page_number не целое число, то
+#         # выдать первую страницу
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # Если page_number находится вне диапазона, то
+#         # выдать последнюю страницу
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'posts': posts})
+
+
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(
+        Post, status=Post.Status.PUBLISHED, slug=slug, publish__year=year, publish__month=month, publish__day=day
+    )
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s ({cd['email']}) comments: {cd['comments']}"
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
