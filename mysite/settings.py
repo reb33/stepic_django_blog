a5d20c1151fb8cd57a5d1c41ac4c0ee6d58a3835
k.barylnikov <ipt_git_bot@ipt.ru>
2025-11-26 14:26:54 +0300

    5.18. переход на postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
