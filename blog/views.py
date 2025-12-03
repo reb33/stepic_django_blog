799a07feed0b1ed133412b81e5f74f9d11756a35
k.barylnikov <ipt_git_bot@ipt.ru>
2025-12-04 00:03:01 +0300

    5.19.2 Взвешивание запросов

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -120,11 +120,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body', config='russian')
-            search_query = SearchQuery(query, config='russian')
-            results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-            ).filter(search=search_query).order_by('-rank')
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(rank=SearchRank(search_vector, search_query))
+                .filter(rank__gte=0.3)
+                .order_by('-rank')
+            )
 
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
