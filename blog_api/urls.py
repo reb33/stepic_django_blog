7d18572ee5f905f338f9fc436f5bc7f207e27c12
k.barylnikov <ipt_git_bot@ipt.ru>
2025-12-20 20:15:48 +0300

    7.4 GenericAPIView и фильтрация

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PostDetail, PostList
+from .views import PostDetail, PostList, UserPostList
 
 urlpatterns = [
     path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('', PostList.as_view(), name='post_list'),
+    path('user/<int:id>/', UserPostList.as_view()), # new
 ]
