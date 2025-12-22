1582cb85a687e0055657edca7f1c75ed8e819b9f
k.barylnikov <ipt_git_bot@ipt.ru>
2025-12-22 12:20:44 +0300

    7.2.1 автозаполнение поля author в сериализаторе

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -4,6 +4,8 @@ from blog.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         fields = (
             "id",
