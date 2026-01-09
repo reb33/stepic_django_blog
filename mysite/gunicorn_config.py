266c7aacbc2d6ceaa29d17976a04460c1b4e5300
k.barylnikov <ipt_git_bot@ipt.ru>
2026-01-09 15:33:12 +0300

    9.2 del gunicorn_config.py

--- mysite/gunicorn_config.py
+++
@@ -1,6 +0,0 @@
-command = '/Users/kbarylnikov/education/stepik/mysite/env/bin/gunicorn'
-python_path = '/Users/kbarylnikov/education/stepik/mysite/mysite'
-bind = '0.0.0.0:8001'
-workers = 5
-user = 'www'
-raw_env = 'DJANGO_SETTINGS_MODULE=mysite.settings'
